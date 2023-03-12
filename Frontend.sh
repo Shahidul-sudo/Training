7213d3da0d03dd65115195cc05b6e657e7fb4000
ShahidDevOps <shahid.dav4@gmail.com>
2023-03-11 19:47:00 -0600

    nginx12th

--- Frontend.sh
+++ Frontend.sh
@@ -7,25 +7,54 @@ if [ "$ID" -ne 0 ] ; then
    exit 1
 fi
 echo -n "Installing Nginx : "
-yum install nginx123 -y &>> /tmp/frontend.log
+yum install nginx -y &>> /tmp/frontend.log
 
 if [ $? -eq 0 ] ; then
     echo -e "\e[32m Success \e[0m"
 else
     echo -e "\e[31m Failure  \e[0m"
+    exit 2
 fi
-
+echo -n "Downloading the frontend component : "
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
+if [ $? -eq 0 ] ; then
+    echo -e "\e[32m Success \e[0m"
+else
+    echo -e "\e[31m Failure  \e[0m"
+    exit 2
+fi
+echo -n "Performing cleanup of old frontend content : " 
 cd /usr/share/nginx/html
 rm -rf * &>> /tmp/frontend.log
+
+if [ $? -eq 0 ] ; then
+    echo -e "\e[32m Success \e[0m"
+else
+    echo -e "\e[31m Failure  \e[0m"
+    exit 2
+fi
+echo "Copying the downloaded frontend content : "
 unzip /tmp/frontend.zip &>> /tmp/frontend.log
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
+if [ $? -eq 0 ] ; then
+    echo -e "\e[32m Success \e[0m"
+else
+    echo -e "\e[31m Failure  \e[0m"
+    exit 2
+fi
+echo -n "Starting the service : "
 systemctl enable nginx &>> /tmp/frontend.log
 systemctl restart nginx &>> /tmp/frontend.log
 
+if [ $? -eq 0 ] ; then
+    echo -e "\e[32m Success \e[0m"
+else
+    echo -e "\e[31m Failure  \e[0m"
+    exit 2
+fi
 
