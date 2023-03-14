fc7197e9c481337c37d366ba3c26662aca61d3cc
ShahidDevOps <shahid.dav4@gmail.com>
2023-03-14 14:06:22 -0500

    redis1

--- redis.sh
+++ redis.sh
@@ -17,11 +17,11 @@ stat(){
     fi   
 }
 echo -n "Confiuring $COMPONENT repo : "
-curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
+curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>> $LOGFILE
 stat $?
 
 echo -n "Installing $COMPONENT server :"
-yum install redis-6.2.11 -y  &>> $LOGFILE
+yum install redis-6.2.11 -y &>> $LOGFILE
 stat $? 
 
 echo -n "Updating the $COMPONENT visibility : "
