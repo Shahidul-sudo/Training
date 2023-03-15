11ec87ef4131669705dce5968379ac75cb09efab
ShahidDevOps <shahid.dav4@gmail.com>
2023-03-15 16:50:16 -0500

    cart

--- cart.sh
+++ cart.sh
@@ -51,7 +51,7 @@ npm install &>> $LOGFILE
 stat $?
 
 echo -n "Updating the systemd file with DB details : "
-sed -i -e  's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/mongodb.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
+sed -i -e  's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
 mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
 stat $?
 
